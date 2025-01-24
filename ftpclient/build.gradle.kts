29b35803113c11e3d389b37ac650abbe09e7ce18
nisath-android <naminfo.chennai@gmail.com>
2025-01-24 15:07:11 +0530

    second

--- ftpclient/build.gradle.kts
+++ ftpclient/build.gradle.kts
@@ -10,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 29
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -24,10 +23,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -37,6 +38,37 @@ publishing {
     publications {
         register<MavenPublication>("release") {
             afterEvaluate { from(components["release"]) }
+
+            groupId = "com.github.nisath-android"
+            artifactId = "ftp-library"
+            version = "1.0.0"
+
+            pom {
+                name.set("FTP Library")
+                description.set("A simple FTP client library for Android.")
+                url.set("https://github.com/nisath-android/ftp-library")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.html")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("nisath-android")
+                        name.set("Nisath")
+                        email.set("naminfo.chennai@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/nisath-android/ftp-library.git")
+                    developerConnection.set("scm:git:ssh://github.com/nisath-android/ftp-library.git")
+                    url.set("https://github.com/nisath-android/ftp-library")
+                }
+            }
         }
     }
 }
